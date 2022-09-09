ba522014f5d32239568c2043c4ab45288d09f85c
Mgibson7 <mgibsonasc@gmail.com>
2022-09-09 13:24:18 -0400

    start of new animation portfolio

---
+++ src/pages/uncw/Uncw.jsx
@@ -0,0 +1,26 @@
+import "./uncw.css"
+
+const Uncw = () => {
+  return (
+    <div className="container">
+        <section>
+          <div className="plane">
+<img src="https://static.vecteezy.com/system/resources/thumbnails/001/208/469/small/airplane.png" alt="" /> 
+<div className="title">MCG COLLEGE YEARS</div>         
+</div>
+          
+            <div className="logo">
+              <img src="https://logos-world.net/wp-content/uploads/2020/07/NC-Wilmington-Seahawks-Emblem.png" alt="" />
+
+            </div>
+           
+            <div className="wave wave1"></div>
+            <div className="wave wave2"></div>
+            <div className="wave wave3"></div>
+            <div className="wave wave4"></div>
+        </section>
+    </div>
+  )
+}
+
+export default Uncw
