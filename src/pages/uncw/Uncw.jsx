fcd16f5565d65af980ba8f021c97da37c662168a
Mgibson7 <mgibsonasc@gmail.com>
2022-09-09 14:34:41 -0400

    added box for info on college achievments

--- src/pages/uncw/Uncw.jsx
+++ src/pages/uncw/Uncw.jsx
@@ -8,6 +8,13 @@ const Uncw = () => {
 <img src="https://static.vecteezy.com/system/resources/thumbnails/001/208/469/small/airplane.png" alt="" /> 
 <div className="title">MCG COLLEGE YEARS</div>         
 </div>
+<div className="info">
+<h5>UNIVERSITY OF NORTH CAROLINA WILMINGTON </h5>
+  <h5>BACHELOR OF SCIENCE</h5>
+  <h5>BUSINESS ADMINISTRATION</h5>
+  <h5>CONCENTRATION ECONOMICS</h5>
+  <h5>GRADUATED 2013</h5>
+</div>
           
             <div className="logo">
               <img src="https://logos-world.net/wp-content/uploads/2020/07/NC-Wilmington-Seahawks-Emblem.png" alt="" />
